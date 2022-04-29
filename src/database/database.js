3a8defb30fbe64128bddc14bb009aa758f46e04b
Edgar Carlos Mauricio Quintero Romero <equintero@sisvida.co>
2022-04-29 02:41:27 -0500

    data

--- src/database/database.js
+++ src/database/database.js
@@ -3,5 +3,5 @@ import Sequelize from 'sequelize';
 export const sequelize = new Sequelize("sis_analytics", "postgres", "Qazwsxx2", {
     host: "localhost",
     dialect: "postgres",
-    port: 5005,
+    // port: 5005,
 });
--- src/index.js
+++ src/index.js
@@ -1,31 +1,46 @@
+import 'dotenv/config';
+
+import app from './app.js';
+import { sequelize } from './database/database.js';
+import { Genero } from './models/genero.js';
+
+// import './models/genero.js';
 // import './models/grupo.js';
 // import './models/municipio.js';
 // import './models/pais.js';
 // import './models/tipo_acceso.js';
 // import './models/tipo_documento.js';
-// import './models/usuario_sis.js';
-// import './models/usuario_sis_acceso.js';
 // import './models/usuario_sis_acceso_pin.js';
+// import './models/usuario_sis_acceso.js';
 // import './models/usuario_sis_documento.js';
 // import './models/usuario_sis_grupo.js';
 // import './models/usuario_sis_recuperar.js';
-import 'dotenv/config';
-
-import app from './app.js';
-import { sequelize } from './database/database.js';
-
-
+// import './models/usuario_sis.js';
 // Settings
 const PORT = process.env.PORT || 5001;
 
 async function main() {
     try {
-        await sequelize.sync({ force: false });
+        // await sequelize.sync();
         // await sequelize.sync();
         //await sequelize.sync({ alter: true });
         //await sequelize.authenticate().then(() => {
         //    console.log("Enlace establecido con la base de datos");
         //});
+        await sequelize.sync({ force: true }).then(() => {
+            console.log(`Base de datos & tablas creadas!`);
+
+            Genero.bulkCreate([
+                { id_genero: 1, descripcion: "hombre", estado: 1 },
+                { id_genero: 2, descripcion: "mujer", estado: 1 },
+            ])
+                .then(function () {
+                    return Genero.findAll();
+                })
+                .then(function (rta) {
+                    console.log(rta);
+                });
+        });
 
         app.get("/", function (req, res) {
             res.send("Hola");
